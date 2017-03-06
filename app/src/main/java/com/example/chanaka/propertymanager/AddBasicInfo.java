f20f49548209cfb6bae0262d19b0a17e690cfdc7
Chanaka-Sandeepa <chanaka.14@cse.mrt.ac.lk>
2017-03-06 18:59:21 +0530

    project started

---
+++ app/src/main/java/com/example/chanaka/propertymanager/AddBasicInfo.java
@@ -0,0 +1,8 @@
+package com.example.chanaka.propertymanager;
+
+/**
+ * Created by chanaka on 3/6/17.
+ */
+
+public class AddBasicInfo {
+}
---
+++ app/src/main/java/com/example/chanaka/propertymanager/AddProperty.java
@@ -0,0 +1,13 @@
+package com.example.chanaka.propertymanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddProperty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_property);
+    }
+}
